ce4058bc10af45c182c658f4b48d200a6674b966
Kamil Mikuła <kamimik090@student.polsl.pl>
2023-07-30 19:32:14 +0200

    ok

---
+++ AngularAspNet/Controllers/ApiExCurrencies.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.ComponentModel;
+
+namespace AngularAspNet.Controllers
+{
+    public class ApiExCurrencies : Controller
+    {
+        //Creating static HttpClient 
+        private static readonly HttpClient client = new HttpClient();
+       
+            
+    public async Task<IActionResult> Index()
+    {
+    HttpResponseMessage response = await client.GetAsync("https://api.exchangerate-api.com/v4/latest/USD");
+    string responseBody = await response.Content.ReadAsStringAsync();
+    //
+    //Creating collection provided with NewtonsoftJson
+    var data = JObject.Parse(responseBody);
+            //st.WriteLine("1 USD is equal to " + data["rates"]["EUR"] + " Euros " + DateTime.Now.ToString());
+            //st.WriteLine("1 USD is equal to " + data["rates"]["PLN"] + " PLN " + DateTime.Now.ToString());
+            //st.WriteLine("1 USD is equal to " + data["rates"]["CHF"] + " CHF " + DateTime.Now.ToString());
+     return View();
+    }
+  }
+}
